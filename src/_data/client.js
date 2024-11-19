ccbb3730c9572ccd406ddb29ca57860caf24f0c8
Gunnar-Hicks <163244030+Gunnar-Hicks@users.noreply.github.com>
2024-11-19 16:45:14 -0500

    Updated Styling
    Changed placeholder information within client.js. Updated page colors. Added logo SVG.

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,14 +1,14 @@
 module.exports = {
-    "name": "Placeholder",
-    "email": "placeholderwebsite.com", //Update this
-    "phoneForTel": "423-421-1568",
+    "name": "Riverview Technology Services",
+    "email": "contact@riverviewtechservices.com", //Update this
+    "phoneForTel": "423-771-1708",
     "phoneFormatted": "(555) 779-4407",
     "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
+        //"lineOne": "First Address Line",
+        //"lineTwo": "Second Address Line",
         "city": "Chattanooga",
         "state": "TN",
-        "zip": "37341",
+        "zip": "37405",
         "country": "US",
         "mapLink": "https://g.co/kgs/k3v43uZ"
     },
@@ -17,5 +17,5 @@ module.exports = {
         "instagram": "",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.placeholderwebsite.com"
+    "domain": "https://www.riverviewtechservices.com"
 };
